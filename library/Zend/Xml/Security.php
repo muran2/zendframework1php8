9e3cd883a2da3ca00095d5ddad5b89378e469c86
muran <ab.muranov@gmail.com>
2021-12-11 00:35:55 +0700

    init

--- library/Zend/Xml/Security.php
+++ library/Zend/Xml/Security.php
@@ -166,17 +166,17 @@ class Zend_Xml_Security
      */
     public static function isPhpFpm()
     {
-        $isVulnerableVersion = (
-            version_compare(PHP_VERSION, '5.5.22', 'lt')
-            || (
-                version_compare(PHP_VERSION, '5.6', 'gte')
-                && version_compare(PHP_VERSION, '5.6.6', 'lt')
-            )
-        );
-
-        if (substr(php_sapi_name(), 0, 3) === 'fpm' && $isVulnerableVersion) {
-            return true;
-        }
+//        $isVulnerableVersion = (
+//            version_compare(PHP_VERSION, '5.5.22', 'lt')
+//            || (
+//                version_compare(PHP_VERSION, '5.6', 'gte')
+//                && version_compare(PHP_VERSION, '5.6.6', 'lt')
+//            )
+//        );
+//
+//        if (substr(php_sapi_name(), 0, 3) === 'fpm' && $isVulnerableVersion) {
+//            return true;
+//        }
         return false;
     }
 
